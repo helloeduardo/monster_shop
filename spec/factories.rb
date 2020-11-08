dac649319d20b94d110db9a1c316e1b0dc8342fd
Eduardo Parra <helloeduardo@icloud.com>
2020-11-07 21:53:48 -0700

    Add factories for users, merchants, and discounts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,30 @@
 FactoryBot.define do
-  
+  factory :discount do
+    rate { 1.5 }
+    quantity { 1 }
+    merchant
+  end
+
+  factory :user do
+    name { Faker::Name.name }
+    address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip { Faker::Address.zip }
+    email { Faker::Internet.email }
+    password { '12345' }
+
+    factory :merchant_employee do
+      role { 1 }
+      merchant
+    end
+  end
+
+  factory :merchant do
+    name { Faker::Company.name }
+    address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip { Faker::Address.zip }
+  end
 end
