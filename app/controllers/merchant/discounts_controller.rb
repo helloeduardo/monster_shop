8d0ebd96635997de10b62f093b522f56474fe4f8
Eduardo Parra <helloeduardo@icloud.com>
2020-11-08 12:18:33 -0700

    Add merchant before action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,60 +1,55 @@
 class Merchant::DiscountsController < Merchant::BaseController
   before_action :set_discount, only: [:show, :edit, :update, :destroy]
+  before_action :set_merchant, only: [:index, :create]
 
-  # GET /discounts
   def index
-    @merchant = current_user.merchant
-    @discounts = Discount.all
+    @discounts = @merchant.discounts
   end
 
-  # GET /discounts/1
   def show
   end
 
-  # GET /discounts/new
   def new
     @discount = Discount.new
   end
 
-  # GET /discounts/1/edit
   def edit
   end
 
-  # POST /discounts
   def create
-    @discount = Discount.new(discount_params)
+    @discount = @merchant.discounts.new(discount_params)
 
     if @discount.save
-      redirect_to @discount, notice: 'Discount was successfully created.'
+      redirect_to merchant_discounts_path, notice: 'Discount was successfully created.'
     else
       generate_flash(@discount)
       render :new
     end
   end
 
-  # PATCH/PUT /discounts/1
   def update
     if @discount.update(discount_params)
-      redirect_to @discount, notice: 'Discount was successfully updated.'
+      redirect_to [:merchant, @discount], notice: 'Discount was successfully updated.'
     else
       generate_flash(@discount)
       render :edit
     end
   end
 
-  # DELETE /discounts/1
   def destroy
     @discount.destroy
     redirect_to discounts_url, notice: 'Discount was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_discount
       @discount = Discount.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    def set_merchant
+      @merchant = current_user.merchant
+    end
+
     def discount_params
       params.require(:discount).permit(:rate, :quantity, :merchant_id)
     end
